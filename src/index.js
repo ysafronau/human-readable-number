cbec6315990312ccc24e148d86673efbd5a4e5ed
Yauheni Safronau <ysafronau26@gmail.com>
2022-04-30 16:45:32 +0200

    feat: fix func toReadeble

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-module.exports = function toReadable (number) {
-  
-}
+const oneDigit = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+];
+
+const twoDigits = [
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+
+const tens = [
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
+
+module.exports = function toReadable(number) {
+    if (number < 10) return oneDigit[number];
+    if (number < 20) return twoDigits[number % 10];
+    if (number < 100 && number % 10 === 0) return tens[number / 10 - 2];
+    if (number < 100 && number % 10 !== 0)
+        return `${tens[Math.floor(number / 10) - 2]} ${oneDigit[number % 10]}`;
+    if (number < 1000 && number % 100 === 0)
+        return `${oneDigit[number / 100]} hundred`;
+    if (number < 1000 && number % 100 < 10)
+        return `${oneDigit[Math.floor(number / 100)]} hundred ${
+            oneDigit[number % 100]
+        }`;
+    if (number < 1000 && number % 100 < 20)
+        return `${oneDigit[Math.floor(number / 100)]} hundred ${
+            twoDigits[(number % 100) % 10]
+        }`;
+    if (number < 1000 && number % 10 === 0)
+        return `${oneDigit[Math.floor(number / 100)]} hundred ${
+            tens[Math.floor((number % 100) / 10) - 2]
+        }`;
+    if (number < 1000)
+        return `${oneDigit[Math.floor(number / 100)]} hundred ${
+            tens[Math.floor((number % 100) / 10) - 2]
+        } ${oneDigit[number % 10]}`;
+};
